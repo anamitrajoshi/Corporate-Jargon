f848894df0a9b25d0c15505a79ebb8af99e410d0
Anamitra <anamitra.joshi@gmail.com>
2025-03-03 22:25:29 +0530

    testing waters idk anymore i will cry

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.get("/questions/:level", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
