31c42fe7b94ae29fe87ecbbce233be85fe14c2ce
Anamitra <anamitra.joshi@gmail.com>
2025-03-03 17:32:36 +0530

    modified paths in vercel.json

--- server.js
+++ server.js
@@ -49,4 +49,3 @@ app.get("/questions/:level", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
