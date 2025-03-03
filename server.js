6a746eaa14c35188f87969776f441d794179c722
Anamitra <anamitra.joshi@gmail.com>
2025-03-03 17:25:30 +0530

    modified server.js

--- server.js
+++ server.js
@@ -49,3 +49,4 @@ app.get("/questions/:level", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
